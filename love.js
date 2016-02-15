39c7352413cc5ff6929753dc5358b62e672cc6cd
Tweety79rw <ryan.wilson2@email.wsu.edu>
2016-02-14 22:42:08 -0800

    fixed bug with arrow, added ability to change name
    To change name add name="<name>" to query in address bar

--- love.js
+++ love.js
@@ -1,8 +1,23 @@
+var parseAddress = function(){
+	var result = {};
+	var queryArray = window.location.search.replace('?','').split('&');
+	queryArray.forEach(function(element){
+		var parts = element.split('=');
+		result[parts[0]] = parts[1];
+	});
+	return result;
+};
+
 $(document).ready(function(){
+	var name = 'Catrina';
+	var parse = parseAddress();
+	if(parse && parse.name && parse.name.length > 0)
+		name = parse.name;
+	$('#name').html(name);
 	var middle = $(window).width()/2;
 	var flash = function(){
 		$('.love-text').css({'display':'block'}).addClass('flash');
-		$('.arrow').delay(3000).animate({"left":($(window).width()-middle/2-100)+'px'},1000);
+		$('.arrow').delay(3000).animate({"left":(middle+250)+'px'},1000);
 	};
 	$('.left-heart').css({'left':'-200px','display':'block'}).animate({left:(middle-190)+'px'},5000).animate({left:(middle-200)+'px'},1000);
 	$('.right-heart').css({'left':$(window).width()+'px','display':'block'}).animate({left:(middle-10)+'px'},5000).animate({left:(middle)+'px'},1000,flash);
